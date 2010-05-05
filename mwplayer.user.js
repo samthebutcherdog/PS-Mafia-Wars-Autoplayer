319666a49285c8899bbdd754a4e36d77545cab8d
crazy.dude.mw <crazy.dude.mw@b986db5e-eebb-11de-8f41-3b8bcad7c4de>
2010-05-05 04:31:34 +0000

    Moved the new message icon next to the city travel menu.

--- mwplayer.user.js
+++ mwplayer.user.js
@@ -38,12 +38,12 @@
 // @exclude     http://facebook.mafiawars.com/mwfb/*#*
 // @exclude     http://facebook.mafiawars.com/mwfb/remote/html_server.php?*xw_controller=freegifts*
 // @version     1.1.36
-// @build       400
+// @build       401
 // ==/UserScript==
 
 var SCRIPT = {
   version: '1.1.36',
-  build: '400',
+  build: '401',
   name: 'inthemafia',
   appID: 'app10979261223',
   appNo: '10979261223',
@@ -7459,6 +7459,11 @@ function customizeMasthead() {
   var mwapTitle = 'MWAP ' + SCRIPT.version + ' (Build ' + SCRIPT.build + ')';
   makeElement('div', mastheadElt, {'style':'position: absolute; top: 20px; right: 10px; text-align: left; font-size: 11px; font-weight: bold; color: white'}).appendChild(document.createTextNode(mwapTitle));
   var menuElt = makeElement('div', mastheadElt, {'id':'ap_menu', 'style':'position: absolute; top: 34px; font-size: 11px; right: 10px; text-align: left;'});
+
+  var newMessageElt = document.evaluate("//div[@style='position: absolute; top: 32px; right: 126px; width: 45px; z-index: 100;']", document, null, XPathResult.ANY_TYPE,null).iterateNext();
+  newMessageElt.style.left = '248px';
+  newMessageElt.style.top = '7px';
+  mastheadElt.appendChild(newMessageElt);
 
   // Change help intructions :D
   var helpElt = xpathFirst('.//div[@onmouseover="instructionopen()"]', innerPageElt);
