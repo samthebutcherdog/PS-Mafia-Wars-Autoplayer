29ad6443a8615aa0d4601b862e86fbe80336493a
cygnum <cygnum@b986db5e-eebb-11de-8f41-3b8bcad7c4de>
2010-05-10 01:35:12 +0000

    Incremented version no for release

--- mwplayer.user.js
+++ mwplayer.user.js
@@ -37,13 +37,13 @@
 // @exclude     http://mwfb.zynga.com/mwfb/remote/html_server.php?*xw_controller=freegifts*
 // @exclude     http://facebook.mafiawars.com/mwfb/*#*
 // @exclude     http://facebook.mafiawars.com/mwfb/remote/html_server.php?*xw_controller=freegifts*
-// @version     1.1.38
-// @build       407
+// @version     1.1.39
+// @build       408
 // ==/UserScript==
 
 var SCRIPT = {
   version: '1.1.38',
-  build: '407',
+  build: '408',
   name: 'inthemafia',
   appID: 'app10979261223',
   appNo: '10979261223',
