652c3fe3fd50dfbe31eb84265f3d2de7e309c824
takesi.akamatsu <takesi.akamatsu@b986db5e-eebb-11de-8f41-3b8bcad7c4de>
2010-06-07 23:32:25 +0000

    Disabled automatic unchecking properties processing when flash is enabled
    Set check to see if Flash is enabled, if not, do not process properties.

--- mwplayer.user.js
+++ mwplayer.user.js
@@ -39,12 +39,12 @@
 // @exclude     http://facebook.mafiawars.com/mwfb/*#*
 // @exclude     http://facebook.mafiawars.com/mwfb/remote/html_server.php?*xw_controller=freegifts*
 // @version     1.1.42
-// @build       441
+// @build       442
 // ==/UserScript==
 
 var SCRIPT = {
   version: '1.1.42',
-  build: '441',
+  build: '442',
   name: 'inthemafia',
   appID: 'app10979261223',
   appNo: '10979261223',
@@ -1861,7 +1861,10 @@ function doAutoPlay () {
   }
 
   // Auto-collect take (limit to level 4 and above)
-  if (GM_getValue('isRunning') && !maxed && hasProps) {
+  if (GM_getValue('flashed')) {
+    addToLog('warning Icon', 'Warning, flash enabled, properties not collected.');
+  }
+  if (GM_getValue('isRunning') && !maxed && hasProps && GM_getValue('flashed',0)) {
     for (var i = 0, iLength = cities.length; i < iLength; ++i) {
       if (level >= cities[i][CITY_LEVEL] &&
           isGMChecked('collectTake' + cities[i][CITY_NAME]) &&
@@ -9000,12 +9003,12 @@ function customizeProps() {
         isGMChecked('collectTakeCuba') ||
         isGMChecked('collectTakeMoscow') ||
         isGMChecked('collectTakeBangkok')) {
-      GM_setValue('autoBuy', 0);
-      GM_setValue('collectTakeNew York', 0);
-      GM_setValue('collectTakeCuba', 0);
-      GM_setValue('collectTakeMoscow', 0);
-      GM_setValue('collectTakeBangkok', 0);
-      addToLog('warning Icon', 'Property functions disabled.');
+//      GM_setValue('autoBuy', 0);
+//      GM_setValue('collectTakeNew York', 0);
+//      GM_setValue('collectTakeCuba', 0);
+//      GM_setValue('collectTakeMoscow', 0);
+//      GM_setValue('collectTakeBangkok', 0);
+      addToLog('warning Icon', 'Warning: Flash enabled.');
       addToLog('updateBad Icon', 'You must disable flash from your browser for MWAP to work on properties. <br>' +
                'Visit <a href="http://userscripts.org/scripts/show/77953">MWAP for Firefox</a> or ' +
                '<a href="http://www.playerscripts.com/index.php?option=com_jumi&fileid=3&Itemid=18">MWAP for Chrome</a> for instructions. ');
