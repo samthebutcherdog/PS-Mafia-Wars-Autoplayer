dedf61f7b21cfd6c947945de480042fafd9adacf
dexter.whitmore <dexter.whitmore@b986db5e-eebb-11de-8f41-3b8bcad7c4de>
2010-06-20 18:53:48 +0000

    Updated JSON & JS files to 1.1.503 to update Chrome extension with latest changes. Apologises for the delayed Chrome Extension update.

--- mwplayer.user.js
+++ mwplayer.user.js
@@ -38,11 +38,11 @@
 // @exclude     http://mwfb.zynga.com/mwfb/remote/html_server.php?*xw_controller=freegifts*
 // @exclude     http://facebook.mafiawars.com/mwfb/*#*
 // @exclude     http://facebook.mafiawars.com/mwfb/remote/html_server.php?*xw_controller=freegifts*
-// @version     1.1.501
+// @version     1.1.503
 // ==/UserScript==
 
 var SCRIPT = {
-  version: '1.1.501',
+  version: '1.1.503',
   name: 'inthemafia',
   appID: 'app10979261223',
   appNo: '10979261223',
