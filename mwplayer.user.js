299b6f95c7485d8fffe5a09f0ee1e05824bee826
kcmcl64 <kcmcl64@b986db5e-eebb-11de-8f41-3b8bcad7c4de>
2010-10-30 09:54:59 +0000

    FS#34 - removed Google code,not required.

--- mwplayer.user.js
+++ mwplayer.user.js
@@ -43,7 +43,7 @@
 // @include     http://www.facebook.com/connect/uiserver*
 // @exclude     http://mwfb.zynga.com/mwfb/*#*
 // @exclude     http://facebook.mafiawars.com/mwfb/*#*
-// @version     1.1.815
+// @version     1.1.816
 // ==/UserScript==
 
 // search for new_header   for changes
@@ -54,7 +54,7 @@
 // once code is proven ok, take it out of testing
 //
 var SCRIPT = {
-  version: '1.1.815',
+  version: '1.1.816',
   name: 'inthemafia',
   appID: 'app10979261223',
   appNo: '10979261223',
@@ -70,16 +70,6 @@ var SCRIPT = {
   ajaxIDSync: 'ajax_sync',    // page for Autoplay ('synchronous')
   ajaxIDAsync: 'ajax_async'   // page for asynchronous clicking (quickbanking, profile icecheck etc)
 };
-
-/** Google Analytics. See where the script is being used and how often. This does not track any individual user in anyway. KCMCL */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-3078135-18']);  /** use PS for tracking uses of MWAP **/
-_gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 // Set the storage path
 var GMSTORAGE_PATH = 'GM_' + SCRIPT.appID + '_';
